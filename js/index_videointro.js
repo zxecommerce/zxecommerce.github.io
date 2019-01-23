6dc92ad3d203a32bb37e7896c4372332b8453d55
andreaslarsen13 <andreas.larsen@zx-ventures.com>
2019-01-23 16:29:34 -0500

    all

---
+++ js/index_videointro.js
@@ -0,0 +1,36 @@
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('video-placeholder', {
+    width: 640,
+    height: 360,
+    videoId: 'AqHqMdRcROQ',
+    events: {
+      onReady: initialize
+    }
+  });
+}
+function initialize(){
+}            
+
+function deployVideo() {
+  jQuery('.mm-product-video-modal-container').addClass('open');
+  setTimeout(function() {
+    jQuery('.mm-product-video-modal').addClass('open');
+    player.playVideo();
+  }, 250);
+}
+
+function destroyVideo() {
+  jQuery('.mm-product-video-modal').removeClass('open');
+  setTimeout(function() {
+    jQuery('.mm-product-video-modal-container').removeClass('open');
+    player.pauseVideo();
+  }, 250);
+}
+
+jQuery('.mm-video-overlay').on('click', function() {
+  destroyVideo();
+});
+
+jQuery('.mm-launch-intro').on('click',function() {
+  deployVideo();
+});
